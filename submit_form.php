07628f0b783f4b93b3e724b6e3fc656133c838ff
Mikhail Malin <mishuk.ibiw@gmail.com>
2026-06-04 15:15:19 +0300

    Submit form fixed

--- submit_form.php
+++ submit_form.php
@@ -2,13 +2,12 @@
 if (!empty($_POST)) {
     $to = "zakaz@akabo.ru";
     $subject = "Новая заявка на сайте";
-    $message = "<b>Клиент:</b> {$_POST['customer']}<br><b>Телефон:</b> <a href=\"tel:{$_POST['phone']}\">{$_POST['phone']}";
+    $message = "Клиент: {$_POST['customer']}\nТелефон: {$_POST['phone']}";
 
-    if (isset($_POST['email'])) $message .= "<br><b>Email:</b> {$_POST['email']}";
-    if (isset($_POST['task'])) $message .= "<br><b>Задача:</b> {$_POST['task']}";
+    if (isset($_POST['email'])) $message .= "\nEmail: {$_POST['email']}";
+    if (isset($_POST['task'])) $message .= "\nЗадача: {$_POST['task']}";
 
     if (mail($to, $subject, $message)) {
-        print_r($_POST);
         echo 'Письмо успешно отправлено!';
     } else {
         echo 'Ошибка при отправке письма.';
